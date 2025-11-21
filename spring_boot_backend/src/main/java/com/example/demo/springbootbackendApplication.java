ba1c3e6145787ceb33f5e7fe30d40cde0718162a
Kavia AI <production@kavia.ai>
2025-11-21 13:15:44 +0000

    Checkpoint 1 - cga707084e

---
+++ spring_boot_backend/src/main/java/com/example/demo/springbootbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springbootbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(springbootbackendApplication.class, args);
+	}
+
+}
